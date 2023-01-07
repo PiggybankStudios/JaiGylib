baed1826655117b2db8ced63bb197ff114f930e1
Taylor Robbins <robbitay@live.com>
2023-01-07 12:34:48 -0800

    Initial Commit - Currently finished: gy_vectors, gy_rectangles, gy_directions, gy_intrinsics/gy_math/gy_polymorphism, gy_constants, gy_assert

---
+++ build/build.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem The "Safe" name doesn't have spaces or invalid characters and also doesn't change for Demo vs non-Demo
+set ProjectName=Jai Gylib Test
+set ProjectNameSafe=JaiGylibTest
+set RunProgram=0
+set CopyToDataDirectory=1
+
+set WorkingDirectory=%cd%
+set LibDirectory=..\lib
+set CodeDirectory=..\code
+set GameDirectory=..\game
+set DataDirectory=..\data
+set TimeString=%date:~-4,4%%date:~-10,2%%date:~-7,2%%time:~0,2%%time:~3,2%%time:~6,2%
+
+set ExeName=%ProjectNameSafe%.exe
+set PdbName=%ProjectNameSafe%.pdb
+
+echo [Compiling "%ProjectName%" in %WorkingDirectory%...]
+
+rem -no_cwd - Stops the compiler from changing the working directory when compiling. This keeps the generated .build folder in our build folder
+jai "%CodeDirectory%\build.jai" -no_dce -output_path "%WorkingDirectory%" -exe "%ExeName%"
+
+rem NOTE: Jai compiler seems to always put the result in the same folder as the code. We need to copy the .exe and .pdb to our build folder manually
+rem MOVE /Y "%CodeDirectory%\%ExeName%" "%ExeName%" > NUL
+rem MOVE /Y "%CodeDirectory%\%PdbName%" "%PdbName%" > NUL
+
+if "%CopyToDataDirectory%"=="1" (
+	echo [Copying %ProjectNameSafe%.exe to data directory]
+	XCOPY ".\%ProjectNameSafe%.exe" "%DataDirectory%\" /Y > NUL
+) else (
+	echo [Build Finished!]
+)
+
+if "%RunProgram%"=="1" (
+	echo [Running Program...]
+	pushd ..\data
+	..\build\%ProjectNameSafe%.exe
+	popd
+)
